04fc208f0408aa5fdd0d6a556a1b575879ef68c0
Marcos Diaz <ghostframez@gmail.com>
2022-10-23 13:17:17 -0300

    Loading...

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,6 +1,10 @@
 const element = document.getElementById("game") as HTMLCanvasElement;
 const context = element.getContext("2d") as CanvasRenderingContext2D;
 
+context.fillStyle = "white"
+context.font = 'Arial 36px'
+context.fillText("loading...", element.width / 2, element.height / 2)
+
 const { width, height } = element;
 
 export const Canvas = {
